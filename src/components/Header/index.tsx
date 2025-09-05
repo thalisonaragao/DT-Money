30b4b74462886420aaaac1da1a195f46028c4f50
Thalison Aragão <thalissonaragao01@gmail.com>
2025-09-05 18:46:50 -0300

    Estrutura visual - Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -9,7 +10,22 @@ export function Header() {
                 <HeaderContent>
                     <img src={logoImg} alt="" />
 
-                    <NewTransactionButton>Nova transação</NewTransactionButton>
+                    <Dialog.Root>
+                        {/*Para aproveitar o botão do NewTransactionButton e não criar um butão dentro de outro */}
+                       <Dialog.Trigger asChild> 
+                         <NewTransactionButton>Nova transação</NewTransactionButton>
+                       </Dialog.Trigger>
+                         {/* Ele deixa o elemento em cima das divs  */}
+                       <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                            {/*Anucia que vai abrir um modal */}
+                            <Dialog.Title>Nova Transação</Dialog.Title> 
+                            <Dialog.Close/>
+                        </Dialog.Content>
+                       </Dialog.Portal>
+
+                    </Dialog.Root>
                 </HeaderContent>
             </HeaderContainer>
         </div>

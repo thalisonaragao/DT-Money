3202e75e5ce7008c2ab7774143aa6ff9a4f367bc
Thalison Aragão <thalissonaragao01@gmail.com>
2025-09-06 15:59:21 -0300

    Estrutura visual - Modal de nova transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -15,16 +16,8 @@ export function Header() {
                        <Dialog.Trigger asChild> 
                          <NewTransactionButton>Nova transação</NewTransactionButton>
                        </Dialog.Trigger>
-                         {/* Ele deixa o elemento em cima das divs  */}
-                       <Dialog.Portal>
-                        <Dialog.Overlay />
-                        <Dialog.Content>
-                            {/*Anucia que vai abrir um modal */}
-                            <Dialog.Title>Nova Transação</Dialog.Title> 
-                            <Dialog.Close/>
-                        </Dialog.Content>
-                       </Dialog.Portal>
-
+                         
+                         <NewTransactionModal />
                     </Dialog.Root>
                 </HeaderContent>
             </HeaderContainer>
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,31 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
+import { Overlay, Content, CloseButton } from './styles'
+import { X } from 'phosphor-react'
+
+export function NewTransactionModal() {
+    return (
+    <Dialog.Portal>
+            {/*Dialog.Portal =  Ele deixa o elemento em cima das divs  */}
+        <Overlay />
+        <Content>
+            <Dialog.Title>Nova Transação</Dialog.Title> 
+
+            <CloseButton>
+                <X size={24}/>
+            </CloseButton>
+
+            <form action=''>
+                <input type="text" placeholder='Descrição' required />
+                <input type="number" placeholder='Preço' required />
+                <input type="text" placeholder='Categoria' required />
+
+                <button type='submit'>
+                    Cadastrar
+                </button>
+            </form>
+        </Content>
+    </Dialog.Portal>
+        
+    )
+}

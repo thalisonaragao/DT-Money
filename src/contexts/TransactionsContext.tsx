5fa7501cd67581a377e4a2117a0b5456390d461c
Thalison Aragão <thalissonaragao01@gmail.com>
2025-11-27 19:56:07 -0300

    Performance - Aplicando o useCallback

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { useCallback, useEffect, useState, type ReactNode } from "react";
 import { createContext } from 'use-context-selector'
 import { api } from "../lib/aixos";
 
@@ -33,20 +33,21 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionProvider ({ children }: TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transaction[]>([])
       
-    async function fetchTransactions(query?:string) {                
+    const fetchTransactions = useCallback(async (query?: string) => {
         const response = await api.get('transactions', {
-            params: {
-                _sort: 'createdAt',
-                _order: 'desc',
-                q: query,
-            }
+        params: {
+            _sort: 'createdAt',
+            _order: 'desc',
+            q: query,
+        },
         })
 
-        setTransactions(response.data) 
-    }
+        setTransactions(response.data)
+    }, [])
 
-    async function createTransaction(data: CreateTransactionInput){
-        const { description, price, category, type } = data;
+    const createTransaction = useCallback(
+        async (data: CreateTransactionInput) => {
+        const { description, price, category, type } = data
 
         const response = await api.post('transactions', {
             description,
@@ -54,10 +55,11 @@ export function TransactionProvider ({ children }: TransactionsProviderProps){
             category,
             type,
             createdAt: new Date(),
-        });
+        })
 
-        setTransactions(state => [response.data, ...state])
-    }
+        setTransactions((state) => [response.data, ...state])
+        },[],
+    )
     
         useEffect(() => {
             fetchTransactions();
